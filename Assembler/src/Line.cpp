1c8ee1493f633aed1a3428b3389fa8073ff1605d
Mohamed Ayman <mystic_sorcerer@hotmail.com>
2017-04-18 23:10:50 +0200

    finish line

--- Assembler/src/Line.cpp
+++ Assembler/src/Line.cpp
@@ -7,12 +7,87 @@
 
 #include "Line.h"
 
-Line::Line() {
-	// TODO Auto-generated constructor stub
-
+Line::Line(std::string line) {
+	parseLine(line);
 }
 
 Line::~Line() {
-	// TODO Auto-generated destructor stub
+
 }
 
+void Line::parseLine(std::string line){
+    ReadState state = ReadState::LABEL;
+    int pos = 0;
+    int len = line.length();
+    while(pos < len){
+        char curChar = line[pos];
+        if(isspace(curChar)){
+            state = getNextState(state);
+            while(pos < len - 1 && isspace(line[pos + 1])) pos++;
+            continue;
+        }
+        switch(state){
+            case ReadState::LABEL:
+                label += curChar;
+                readLabel = true;
+                break;
+            case ReadState::OPERATION:
+                operation += curChar;
+                readOperation = true;
+                break;
+            case ReadState::OPERAND:
+                operand += curChar;
+                readOperand = true;
+                break;
+            case ReadState::COMMENT:
+                for(; pos < len; pos++)
+                    comment += line[pos];
+                readComment = true;
+        }
+    }
+}
+
+ReadState Line::getNextState(ReadState curState){
+    switch(curState){
+        case ReadState::LABEL:
+            return ReadState::OPERATION;
+        case ReadState::OPERATION:
+            return ReadState::OPERAND;
+        case ReadState::OPERAND:
+            return ReadState::COMMENT;
+        default:
+            return ReadState::INVALID;
+    }
+}
+
+bool Line::hasLabel(){
+    return readLabel;
+}
+
+bool Line::hasOperation() {
+    return readOperation;
+}
+
+bool Line::hasOperand() {
+    return readOperand;
+}
+
+bool Line::hasComment() {
+    return readComment;
+}
+
+std::string Line::getLabel() {
+    return label;
+}
+
+std::string Line::getOperation() {
+    return operation;
+}
+
+std::string Line::getOperand() {
+    return operand;
+}
+
+std::string Line::getComment() {
+    return comment;
+}
--- Assembler/src/Line.h
+++ Assembler/src/Line.h
@@ -1,17 +1,30 @@
-/*
- * Line.h
- *
- *  Created on: Apr 16, 2017
- *      Author: mahmoud
- */
-
 #ifndef LINE_H_
 #define LINE_H_
 
+#include <string>
+
+enum class ReadState { LABEL, OPERATION, OPERAND, COMMENT, INVALID};
+
 class Line {
 public:
-	Line();
+	Line(std::string line);
 	virtual ~Line();
+
+	bool hasLabel();
+	bool hasOperation();
+	bool hasOperand();
+	bool hasComment();
+	std::string getLabel();
+	std::string getOperation();
+	std::string getOperand();
+	std::string getComment();
+	ReadState getNextState(ReadState);
+
+private:
+	std::string label, operation, operand, comment;
+	bool readLabel, readOperation, readOperand, readComment;
+
+	void parseLine(std::string line);
 };
 
 #endif /* LINE_H_ */
