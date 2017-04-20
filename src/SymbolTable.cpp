02e8545c530190573d863a7fca22c04721772ee9
Mohamed Ayman <mystic_sorcerer@hotmail.com>
2017-04-20 23:49:23 +0200

    fix case sensitivity

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,6 +1,6 @@
 #include "SymbolTable.h"
 #include <unordered_map>
-
+#include <algorithm>
 SymbolTable::SymbolTable() {
 
 }
@@ -10,14 +10,17 @@ SymbolTable::~SymbolTable() {
 }
 
 bool SymbolTable::hasLabel(std::string label) {
+    std::transform(label.begin(), label.end(), label.begin(), ::tolower);
     return table.count(label);
 }
 
 void SymbolTable::addLabel(std::string label, int address) {
+    std::transform(label.begin(), label.end(), label.begin(), ::tolower);
     table[label] = address;
 }
 
 int SymbolTable::getAddress(std::string label){
+    std::transform(label.begin(), label.end(), label.begin(), ::tolower);
     return table[label];
 }
 
